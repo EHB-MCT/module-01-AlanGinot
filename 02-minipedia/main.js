287b3d3fec089e6bd4afe1b0be55c33641fc7974
AlanGinot <alan.ginot@student.ehb.be>
2026-02-12 12:25:38 +0100

    Second Exercise First module

--- 02-minipedia/main.js
+++ 02-minipedia/main.js
@@ -9,11 +9,33 @@ let subtitleFR = "Un fruit délicieux";
 let textFR = "La pomme est un fruit comestible produit par un pommier. Les pommes sont riches en fibres et en vitamine C. Il existe des milliers de variétés, allant du vert et jaune au rouge foncé.";
 
 // 1. Select text elements
-
+let title = document.querySelector("#title");
+let subtitle = document.querySelector("#subtitle");
+let text = document.querySelector("#text");
 //2. Set initial content on load
-
+title.innerText =titleNL;
+subtitle.innerText =subtitleNL;
+text.innerText=textNL;
 //3. Select buttons
-
+let nlButton = document.querySelector("#btn-nl");
+let frButton = document.querySelector("#btn-fr");
 //4. Add event listeners
-
+nlButton.addEventListener('click',NLTEXT);
+frButton.addEventListener('click',FRTEXT);
+function NLTEXT()
+{
+title.innerText =titleNL;
+subtitle.innerText =subtitleNL;
+text.innerText=textNL;
+}
+function FRTEXT()
+{
+title.innerText =titleFR;
+subtitle.innerText =subtitleFR;
+text.innerText=textFR;
+}
 //5. Create, configure and append image
+let img= document.createElement("img");
+img.src ="appel.jpg";
+document.body.appendChild(img);
+
